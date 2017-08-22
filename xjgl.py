05d8576ec0e87fdd0c888e5c4f5c95d069d39767
bg <bg@example.com>
2017-08-22 15:45:50 +0800

    fix bug! init 数值错误

--- xjgl.py
+++ xjgl.py
@@ -11,15 +11,22 @@ import WorkInTime
 class Xjgl(object):
     __highIn = 0
 
-    def __init__(self, highIn):
+    def __init__(self, highIn, setDay = now.day):
         self.__highIn = highIn
+        self.__initHigh = highIn
+        self.__setDay = setDay
+
+    def setInitHigh(self, highIn):
         self.__initHigh = highIn
 
     def reset(self):
         self.__highIn = self.__initHigh
+        self.__setDay = now.day
 
     def WatchXjgl(self):
         sendString = ''
+        if now.day != self.__setDay:
+            self.reset()
         try:
             check_seesion = requests.Session()
             url = 'https://www.jisilu.cn/data/repo/sz_repo_list/?___t=1489544161142'
@@ -32,25 +39,29 @@ class Xjgl(object):
         #for row in jsonXjgl['rows']:
         row = jsonXjgl['rows'][0]
         #print(row)
-        rowHigh = float(row['cell']['daily_profit2'])
+        rowHigh = float(row['cell']['price'])
         if rowHigh > self.__highIn:    #新高超过前基准
-            sub = '逆回购: ' + row['id'] + ' 破 ' + str(self.__highIn) + ', 现日年化: ' + row['cell']['daily_profit2']
+            sub = '逆回购: ' + row['id'] + ' 破 ' + str(self.__highIn) + ', 现价: ' + row['cell']['price']
             self.__highIn = max(self.__highIn * 1.3, rowHigh)
             print(sub)
-            #sendMail.sendMail(sub, "")
+            sendMail.sendMail(sub, "")
 
-xjglWatch = Xjgl(4)
+
 now = datetime.now()
 nowDay = now.day
-
+priceIn = 4.6
 print("现金管理正在运行")
 timeTrade = [['9:29', '11:30'], ['13:00', '15:00']]
-workTime = WorkInTime.WorkInTime(timeTrade)
+workTime = WorkInTime.WorkInTime(timeTrade, weekday='0,1,2,3,4')
+
+xjglWatch = Xjgl(priceIn)
+
 while True:
-    if now.weekday() < 5:
-        if workTime.isNewDay():
-            xjglWatch.reset()
-        #print(datetime.now())
-        xjglWatch.WatchXjgl()
-        workTime.relax()
+    if now.weekday() ==4:
+        xjglWatch.setInitHigh(priceIn*3)
+    else:
+        xjglWatch.setInitHigh(priceIn)
+    #print(datetime.now())
+    xjglWatch.WatchXjgl()
+    workTime.relax()
 
